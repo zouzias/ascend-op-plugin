172cbff3f8c0009d732e30713fca2b46c6907822
Rayyyy <Uedalan@163.com>
2023-04-18 11:27:19 +0800

    Add gather、dot.

---
+++ op_plugin/ops/official/LinspaceKernelNpu.cpp
@@ -0,0 +1,81 @@
+// Copyright (c) 2023 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/ops/OpInterface.h"
+#include "op_plugin/utils/OpAdapter.h"
+
+namespace op_plugin {
+using npu_preparation = at_npu::native::OpPreparation;
+using npu_utils = at_npu::native::NpuUtils;
+
+at::Tensor& linspace_out(const at::Scalar& start, const at::Scalar& end, int64_t steps, at::Tensor& result) {
+  TORCH_CHECK(steps >= 0, "number of steps must be non-negative");
+
+  if (result.numel() != steps) {
+    result.resize_({steps});
+  }
+  at::Tensor contiguous_result = result.is_contiguous() ? result : result.contiguous();
+  contiguous_result = op_plugin::npu_dtype_cast(contiguous_result, at::kFloat);
+  if(steps == 0){
+    // skip
+  } else if (steps == 1) {
+    contiguous_result.fill_(start);
+  } else {
+    c10::SmallVector<int64_t, N> size_vec = {steps};
+    at_npu::native::OpCommand cmd;
+    cmd.Name("LinSpace")
+        .Input(start, at::ScalarType::Float)
+        .Input(end, at::ScalarType::Float)
+        .Input(size_vec, at::ScalarType::Int)
+        .Output(contiguous_result)
+        .Run();
+  }
+
+  if(contiguous_result.dtype() != result.dtype()) {
+    contiguous_result = contiguous_result.to(result.dtype());
+  }
+
+  return result.copy_(contiguous_result);
+}
+
+at::Tensor linspace(const at::Scalar& start,const at::Scalar& end,
+    int64_t steps,
+    c10::optional<at::ScalarType> dtype_opt,
+    c10::optional<at::Layout> layout_opt,
+    c10::optional<at::Device> device_opt,
+    c10::optional<bool> pin_memory_opt) {
+  TORCH_CHECK(steps >= 0, "number of steps must be non-negative");
+
+  auto device = c10::device_or_default(device_opt);
+  at::TensorOptions option;
+  option = option.dtype(dtype_opt)
+                 .layout(layout_opt)
+                 .device(device)
+                 .pinned_memory(pin_memory_opt);
+
+  // construct the output tensor of the NPU
+  at::Tensor result = npu_preparation::ApplyTensorWithFormat({steps}, option, ACL_FORMAT_ND);
+  at::Tensor resultCast = op_plugin::npu_dtype_cast(result, at::kFloat);
+
+  // calculate the output result of the NPU
+  op_plugin::linspace_out(start, end, steps, resultCast);
+
+  if(option.dtype() != resultCast.dtype()) {
+    resultCast = resultCast.to(option.dtype());
+  }
+
+  return resultCast;
+}
+}  // op_plugin
