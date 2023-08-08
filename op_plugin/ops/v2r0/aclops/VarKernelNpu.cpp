435f49b3d2c6b2ecd76dbcec673fe04668d48dbf
YUANCHUN <chenyuanchun1@huawei.com>
2023-08-08 15:05:36 +0800

    Add 1.11/2.0 api in var.

---
+++ op_plugin/ops/v2r0/aclops/VarKernelNpu.cpp
@@ -0,0 +1,121 @@
+// Copyright (c) 2023 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/ops/OpInterface.h"
+#include "op_plugin/utils/OpAdapter.h"
+#include "op_plugin/utils/custom_functions/aclops/inner_compute.h"
+
+namespace op_plugin {
+at::Tensor& var_out(
+    const at::Tensor& self,
+    at::OptionalIntArrayRef dim,
+    c10::optional<int64_t> correction,
+    bool keepdim,
+    at::Tensor& result) {
+  bool unbiased = !(correction.has_value() && correction.value() == 0);
+  int64_t real_correction = correction.has_value() ? correction.value() : 1;
+  at::IntArrayRef dim_now = dim.value_or(at::IntArrayRef{});
+  return cal_var_out(self, dim_now, real_correction, unbiased, keepdim, result);
+}
+
+at::Tensor& var_out(
+    const at::Tensor& self,
+    at::DimnameList dim,
+    c10::optional<int64_t> correction,
+    bool keepdim,
+    at::Tensor& result) {
+  return at::var_out(result, self, dimnames_to_positions(self, dim), correction, keepdim);
+}
+
+at::Tensor& var_out(
+    const at::Tensor& self,
+    at::DimnameList dim,
+    bool unbiased,
+    bool keepdim,
+    at::Tensor& result) {
+  return at::var_out(result, self, dimnames_to_positions(self, dim), unbiased, keepdim);
+}
+
+at::Tensor& var_out(
+    const at::Tensor& self,
+    at::OptionalIntArrayRef dim,
+    bool unbiased,
+    bool keepdim,
+    at::Tensor& result) {
+  return at::var_out(result, self, at::OptionalIntArrayRef(dim), c10::make_optional<int64_t>({unbiased ? 1 : 0}), keepdim);
+}
+
+at::Tensor var(
+    const at::Tensor& self,
+    at::OptionalIntArrayRef dim,
+    c10::optional<int64_t> correction,
+    bool keepdim) {
+  at::IntArrayRef dim = dim.value_or(at::IntArrayRef{});
+  bool unbiased = !(correction.has_value() && correction.value() == 0);
+  int64_t real_correction = correction.has_value() ? correction.value() : 1;
+  return cal_var(self, dim, real_correction, unbiased, keepdim);
+}
+
+at::Tensor var(
+    const at::Tensor& self,
+    at::DimnameList dim,
+    c10::optional<int64_t> correction,
+    bool keepdim) {
+  return at::var(self, dimnames_to_positions(self, dim), correction, keepdim);
+}
+
+at::Tensor var(const at::Tensor& self, bool unbiased) {
+  return at::var(self, c10::nullopt, c10::make_optional<int64_t>({unbiased ? 1 : 0}));
+}
+
+at::Tensor var(
+    const at::Tensor& self,
+    at::OptionalIntArrayRef dim,
+    bool unbiased,
+    bool keepdim) {
+  return at::var(self, at::OptionalIntArrayRef(dim), c10::make_optional<int64_t>({unbiased ? 1 : 0}), keepdim);
+}
+
+std::tuple<at::Tensor, at::Tensor> NPUNativeFunctions::var_mean(
+    const at::Tensor& self,
+    at::OptionalIntArrayRef dim,
+    c10::optional<int64_t> correction,
+    bool keepdim) {
+  at::IntArrayRef dim_now = dim.value_or(at::IntArrayRef{});
+  bool unbiased = !(correction.has_value() && correction.value() == 0);
+  int64_t real_correction = correction.has_value() ? correction.value() : 1;
+  return cal_var_mean(self, dim_now, unbiased, correction, keepdim);
+}
+
+std::tuple<at::Tensor, at::Tensor> var_mean(
+    const at::Tensor& self,
+    at::DimnameList dim,
+    c10::optional<int64_t> correction,
+    bool keepdim) {
+  return at::var_mean(self, dimnames_to_positions(self, dim), correction, keepdim);
+}
+
+std::tuple<at::Tensor, at::Tensor> var_mean(const at::Tensor& self, bool unbiased) {
+  return at::var_mean(self, c10::nullopt, c10::make_optional<int64_t>({unbiased ? 1 : 0}));
+}
+
+std::tuple<at::Tensor, at::Tensor> var_mean(
+    const at::Tensor& self,
+    at::OptionalIntArrayRef dim,
+    bool unbiased,
+    bool keepdim) {
+  return at::var_mean(self, at::OptionalIntArrayRef(dim), c10::make_optional<int64_t>({unbiased ? 1 : 0}), keepdim);
+}
+} // namespace op_plugin
---
+++ op_plugin/ops/v2r1/aclops/VarKernelNpu.cpp
@@ -0,0 +1,122 @@
+// Copyright (c) 2023 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/ops/OpInterface.h"
+#include "op_plugin/utils/OpAdapter.h"
+#include "op_plugin/utils/custom_functions/aclops/inner_compute.h"
+
+namespace op_plugin {
+at::Tensor& var_out(
+    const at::Tensor& self,
+    at::OptionalIntArrayRef dim,
+    const c10::optional<c10::Scalar>& correction,
+    bool keepdim,
+    at::Tensor& result) {
+  bool unbiased = !(correction.has_value() && correction.value().toInt() == 0);
+  int real_correction = correction.has_value() ? correction.value().toInt() : 1;
+  at::IntArrayRef dim = dim.value_or(at::IntArrayRef{});
+  return cal_var_out(self, dim, real_correction, unbiased, keepdim, result);
+}
+
+at::Tensor& var_out(
+    const at::Tensor& self,
+    at::DimnameList dim,
+    const c10::optional<c10::Scalar>& correction,
+    bool keepdim,
+    at::Tensor& result) {
+  return at::var_out(result, self, dimnames_to_positions(self, dim), correction, keepdim);
+}
+
+at::Tensor& var_out(
+    const at::Tensor& self,
+    at::DimnameList dim,
+    bool unbiased,
+    bool keepdim,
+    at::Tensor& result) {
+  return at::var_out(result, self, dimnames_to_positions(self, dim), c10::make_optional<c10::Scalar>(unbiased ? 1 : 0),
+      keepdim);
+}
+
+at::Tensor& var_out(
+    const at::Tensor& self,
+    at::OptionalIntArrayRef dim,
+    bool unbiased,
+    bool keepdim,
+    at::Tensor& result) {
+  return at::var_out(result, self, at::OptionalIntArrayRef(dim),, c10::make_optional<c10::Scalar>(unbiased ? 1 : 0), keepdim);
+}
+
+at::Tensor var(
+    const at::Tensor& self,
+    at::OptionalIntArrayRef dim,
+    const c10::optional<c10::Scalar>& correction,
+    bool keepdim) {
+  at::IntArrayRef dim_now = dim.value_or(at::IntArrayRef{});
+  bool unbiased = !(correction.has_value() && correction.value().toInt() == 0);
+  int64_t real_correction = correction.has_value() ? correction.value().toInt() : 1;
+  return cal_var(self, dim_now, real_correction, unbiased, keepdim); 
+}
+
+at::Tensor var(
+    const at::Tensor& self,
+    at::DimnameList dim,
+    const c10::optional<c10::Scalar>& correction,
+    bool keepdim) {
+  return at::var(self, dimnames_to_positions(self, dim), correction, keepdim);
+}
+
+at::Tensor var(const at::Tensor& self, bool unbiased) {
+  return at::var(self, c10::nullopt, c10::make_optional<c10::Scalar>(unbiased ? 1 : 0));
+}
+
+at::Tensor var(
+    const at::Tensor& self,
+    at::OptionalIntArrayRef dim,
+    bool unbiased,
+    bool keepdim) {
+  return at::var(self, at::OptionalIntArrayRef(dim), c10::make_optional<c10::Scalar>(unbiased ? 1 : 0), keepdim);
+}
+
+std::tuple<at::Tensor, at::Tensor> var_mean(
+    const at::Tensor& self,
+    at::OptionalIntArrayRef dim,
+    const c10::optional<c10::Scalar>& correction,
+    bool keepdim) {
+  at::IntArrayRef dim_now = dim.value_or(at::IntArrayRef{});
+  bool unbiased = !(correction.has_value() && correction.value().toInt() == 0);
+  int64_t real_correction = correction.has_value() ? correction.value().toInt() : 1;
+  return cal_var_mean(self, dim_now, unbiased, real_correction, keepdim);
+}
+
+std::tuple<at::Tensor, at::Tensor> var_mean(
+    const at::Tensor& self,
+    at::DimnameList dim,
+    const c10::optional<c10::Scalar>& correction,
+    bool keepdim) {
+  return at::var_mean(self, dimnames_to_positions(self, dim), correction, keepdim);
+}
+
+std::tuple<at::Tensor, at::Tensor> var_mean(const at::Tensor& self, bool unbiased) {
+  return at::var_mean(self, c10::nullopt, c10::make_optional<>(unbiased ? 1 : 0));
+}
+
+std::tuple<at::Tensor, at::Tensor> var_mean(
+    const at::Tensor& self,
+    at::OptionalIntArrayRef dim,
+    bool unbiased,
+    bool keepdim) {
+  return at::var_mean(self, at::OptionalIntArrayRef(dim), c10::make_optional<c10::Scalar>(unbiased ? 1 : 0), keepdim);
+}
+} // namespace op_plugin
