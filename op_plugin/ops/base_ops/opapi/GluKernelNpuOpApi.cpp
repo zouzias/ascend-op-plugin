3ac68d6d07bf311fde72a5737f0973c1d3e6ad31
赖长铃 <laichangling@huawei.com>
2023-08-11 07:17:03 +0000

    !169 add op utils * update calcu_op_utils

--- op_plugin/ops/base_ops/opapi/GluKernelNpuOpApi.cpp
+++ op_plugin/ops/base_ops/opapi/GluKernelNpuOpApi.cpp
@@ -25,10 +25,10 @@ using npu_preparation = at_npu::native::OpPreparation;
 
 at::Tensor &glu_out(const at::Tensor &self, int64_t dim, at::Tensor &result) {
   // DO_COMPATIBILITY(aclnnGlu, NPUNativeFunctions::glu_out(self, dim, result));
-  auto output_size = glu_npu_output_size(self, dim);
+  auto output_size = op_infer::glu_npu_output_size(self, dim);
 
   // the dtype that does not check self must be equal to the result dtype
-  npu_preparation::check_tensor({self}, result, result.scalar_type(), output_size);
+  npu_preparation::check_tensor({self}, result, output_size);
 
   // calculate the output result of the NPU
   EXEC_NPU_CMD(aclnnGlu, self, dim, result);
@@ -37,7 +37,7 @@ at::Tensor &glu_out(const at::Tensor &self, int64_t dim, at::Tensor &result) {
 
 at::Tensor glu(const at::Tensor &self, int64_t dim) {
   // DO_COMPATIBILITY(aclnnGlu, NPUNativeFunctions::glu(self, dim));
-  auto output_size = glu_npu_output_size(self, dim);
+  auto output_size = op_infer::glu_npu_output_size(self, dim);
   at::Tensor result = npu_preparation::apply_tensor_without_format(self, output_size);
 
   // calculate the output result of the NPU
