f34c41b741a37ff5049806a1aac9524cf956d87e
胡志坚 <huzhijian3@hisilicon.com>
2023-09-18 09:56:13 +0000

    update op_plugin/ops/v2r1/opapi/IncreFlashAttentionKernelNpuOpApi.cpp.
    Signed-off-by: 胡志坚 <huzhijian3@hisilicon.com>

--- op_plugin/ops/v2r1/opapi/IncreFlashAttentionKernelNpuOpApi.cpp
+++ op_plugin/ops/v2r1/opapi/IncreFlashAttentionKernelNpuOpApi.cpp
@@ -24,7 +24,7 @@ at::Tensor npu_incre_flash_attention(
   at::TensorList keyTensors = {key};
   at::TensorList valueTensors = {value};
 
-  auto actSeqLen = (actual_seq_lengths.has_value()) ? actual_seq_lengths.value().vec() : std::vector<at::IntArrayRef::value::type>{};
+  auto actSeqLen = (actual_seq_lengths.has_value()) ? actual_seq_lengths.value().vec() : std::vector<at::IntArrayRef::value_type>{};
 
   // dispatch hostAPI
   EXEC_NPU_NO_FORMAT_CHECK_CMD(aclnnIncreFlashAttention, query, key, value, padding_mask, atten_mask, s,
