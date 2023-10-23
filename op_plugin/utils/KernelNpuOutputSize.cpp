34a77e5f2a14260b8d1c9fd938250ad1d9773224
liyefeng803@huawei.com <liyefeng803@huawei.com>
2023-10-23 09:38:08 +0800

    test

--- op_plugin/utils/KernelNpuOutputSize.cpp
+++ op_plugin/utils/KernelNpuOutputSize.cpp
@@ -1110,6 +1110,17 @@ c10::SmallVector<int64_t, SIZE> topk_npu_output_size(const at::Tensor &self, int
     return shape;
 }
 
+c10::SmallVector<int64_t, SIZE> rms_norm_npu_output_size(const at::Tensor &self, const at::Tensor &gamma, float eplision)
+{
+    auto x_shape = array_to_small_vector(self.sizes());
+    c10::SmallVector<int64_t, SIZE> rstd_shape;
+    x_dim_num = self.dim();
+    gamma_dim_num = gamma.dim();
+    rstd_dim_num = x_dim_num - gamma_dim_num;
+    rstd_shape = x_shape[: rstd_dim_num - 1]
+    return x_shape, rstd_shape;
+}
+
 c10::SmallVector<int64_t, SIZE> transpose_npu_output_size(const at::Tensor &self, c10::IntArrayRef perm)
 {
     auto sizes = self.sizes();
