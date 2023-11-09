d69802fedef40664a0c5e8331bc995664c1025e7
yayahello <zhaopenglei@hisilicon.com>
2023-11-09 21:35:18 +0800

    Pytorch 2.0.1 Fit FA OP

--- op_plugin/ops/v2r0/opapi/FlashAttentionKernelNpuOpApi.cpp
+++ op_plugin/ops/v2r0/opapi/FlashAttentionKernelNpuOpApi.cpp
@@ -352,4 +352,52 @@ std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor, int64_t, int64_t, int
     return std::make_tuple(attention_score, softmax_max, softmax_sum, softmax_out,
         seed, offset, numels);
 }
+
+at::Tensor npu_prompt_flash_attention(
+    const at::Tensor &query, const at::Tensor &key, const at::Tensor &value,
+    const c10::optional<at::Tensor> &padding_mask,
+    const c10::optional<at::Tensor> &atten_mask,
+    c10::OptionalIntArrayRef actual_seq_lengths,
+    int64_t num_heads, double scale_value,
+    int64_t pre_tokens, int64_t next_tokens,
+    c10::string_view input_layout, int64_t num_key_value_heads)
+{
+    // construct the output tensor of the NPU
+    auto output = npu_preparation::apply_tensor_without_format(query);
+
+    // convert str
+    std::string input_layout_str = std::string(input_layout);
+    char *input_layout_ptr = const_cast<char *>(input_layout_str.c_str());
+
+    auto actSeqLen = actual_seq_lengths.value_or(at::IntArrayRef{});
+
+    // dispatch hostAPI
+    EXEC_NPU_NO_FORMAT_CHECK_CMD(aclnnPromptFlashAttention, query, key, value, padding_mask, atten_mask, actSeqLen,
+                                 num_heads, scale_value, pre_tokens, next_tokens, input_layout_ptr, num_key_value_heads, output);
+    return output;
+}
+
+at::Tensor npu_incre_flash_attention(
+    const at::Tensor &query, const at::Tensor &key, const at::Tensor &value,
+    const c10::optional<at::Tensor> &padding_mask, const c10::optional<at::Tensor> &atten_mask,
+    c10::OptionalIntArrayRef actual_seq_lengths,
+    int64_t num_heads, double scale_value, c10::string_view input_layout, int64_t num_key_value_heads)
+{
+    // construct the output tensor of the NPU
+    auto output = npu_preparation::apply_tensor_without_format(query);
+
+    // convert str
+    std::string input_layout_str = std::string(input_layout);
+    char *input_layout_ptr = const_cast<char *>(input_layout_str.c_str());
+
+    at::TensorList keyTensors = key;
+    at::TensorList valueTensors = value;
+
+    auto actSeqLen = actual_seq_lengths.value_or(at::IntArrayRef{});
+
+    // dispatch hostAPI
+    EXEC_NPU_NO_FORMAT_CHECK_CMD(aclnnIncreFlashAttention, query, keyTensors, valueTensors, padding_mask, atten_mask, actSeqLen,
+                                 num_heads, scale_value, input_layout_ptr, num_key_value_heads, output);
+    return output;
+}
 } // namespace op_api
