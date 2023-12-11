0a34456df49eb874653d07bb773f9548e2f1bd65
lilupu <lilupu1@huawei.com>
2023-12-11 19:32:45 +0800

    fix testcode

--- test/test_base_ops/test_replication_pad3d_backward.py
+++ test/test_base_ops/test_replication_pad3d_backward.py
@@ -1,13 +1,10 @@
-import math
-import unittest
 import numpy as np
 import torch
-
 import torch_npu
 from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import create_common_tensor
 
 torch.npu.set_compile_mode(jit_compile=False)
-DEVICE_NAME = torch_npu.npu.get_device_name(0)[:10]
 
 
 class TestReplicationPad3dBackward(TestCase):
@@ -22,22 +19,19 @@ class TestReplicationPad3dBackward(TestCase):
         grad_result = self_tensor.grad
         return grad_result
 
-    def get_input_data(self, device):
-        input_shape = [1, 1, 4, 4, 4]
-        output_shape = [1, 1, 8, 8, 8]
-        padding = [2, 2, 2, 2, 2, 2]
-
-        self_tensor = torch.ones(input_shape, device=device, dtype=torch.float32)
-        grad_out_tensor = torch.ones(output_shape, device=device, dtype=torch.float32)
-
-        return grad_out_tensor, self_tensor, padding
-
     def test_replication_pad3d_backward(self):
-        grad_out_tensor, self_tensor, padding = self.get_input_data('cpu')
-        grad_out_tensor_npu, self_tensor_npu, padding_npu = self.get_input_data('npu')
+        dtype = np.float32
+        data_format = -1
+        input_shape = [dtype, data_format, [1, 1, 4, 4, 4]]
+        grad_shape = [dtype, data_format, [1, 1, 8, 8, 8]]
+        padding = [2, 2, 2, 2, 2, 2]
+        grad_out_tensor = create_common_tensor(grad_shape, -1, 1)[0]
+        self_tensor = create_common_tensor(input_shape, -1, 1)[0]
+
+        self_tensor_npu = self_tensor.clone().npu()
 
         golden = self.replication_pad3d_backward(grad_out_tensor, self_tensor, padding)
-        output = self.replication_pad3d_backward(grad_out_tensor_npu, self_tensor_npu, padding)
+        output = self.replication_pad3d_backward(grad_out_tensor.npu(), self_tensor_npu, padding)
 
         self.assertRtolEqual(golden, output)
 
