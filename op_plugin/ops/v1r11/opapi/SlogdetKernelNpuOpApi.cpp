a6d81e17260ef9e009b28370ca05efedbd9c8ed8
AllenYaWang <leiwang5@huawei.com>
2024-01-29 22:54:52 +0800

    add PTA1.1 for aclnnInplaceScatterUpdate

--- op_plugin/ops/v1r11/opapi/SlogdetKernelNpuOpApi.cpp
+++ op_plugin/ops/v1r11/opapi/SlogdetKernelNpuOpApi.cpp
@@ -14,7 +14,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "op_plugin/AclOpsInterface.h"
 #include "op_plugin/OpApiInterface.h"
 #include "op_plugin/utils/op_api_common.h"
 
