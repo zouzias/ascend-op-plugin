ab5e010656c3ddb6f21878184ae21212967bfd09
zhuweichen <zhuweichen2@huawei.com>
2024-02-19 16:52:58 +0800

    1

--- op_plugin/ops/v1r11/opapi/IndexKernelNpuOpApi.cpp
+++ op_plugin/ops/v1r11/opapi/IndexKernelNpuOpApi.cpp
@@ -47,6 +47,9 @@ at::Tensor index(const at::Tensor& self, const torch::List<c10::optional<at::Ten
     return at::native::index(self, orig);
   }
   op_plugin::AdvanceIndex::checkIndexTensorTypes(orig);
+  for (c10::optional<at::Tensor> tensor : indices) {
+    std::cout << "index type = " << tensor->scalar_type() << std::endl;
+  }
   auto indices = op_plugin::AdvanceIndex::npu_expand_tensors(self, orig);
   return index_high_dims_op_api(self, indices);
 }
