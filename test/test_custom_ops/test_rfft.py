420bdb3a7d7996f21056b9b6220e807e5e7e0fb3
tangpingchuan <tangpingchuan@huawei.com>
2024-03-25 20:11:59 +0800

    dd

--- test/test_custom_ops/test_rfft.py
+++ test/test_custom_ops/test_rfft.py
@@ -7,7 +7,7 @@ from torch_npu.testing.common_utils import create_common_tensor
 
 class TestRFFT(TestCase):
     def test_npu_rfft_meta(self):
-        shape = [64,64,1024]
+        shape = [64, 64, 1024]
         length = shape[-1]
         x = torch.randn(shape, dtype=torch.float32).npu()
         res = torch.fft.rfft(x, length, norm="backward")
