8ce408f3b92adc0a23b89bc2d23e616a9be84c3d
zhanhao <zhanhao9@huawei.com>
2024-04-24 10:30:48 +0800

    test

--- op_plugin/ops/base_ops/opapi/AddCustomKernelNpuOpApi.cpp
+++ op_plugin/ops/base_ops/opapi/AddCustomKernelNpuOpApi.cpp
@@ -21,40 +21,44 @@ namespace op_api {
 using npu_preparation = at_npu::native::OpPreparation;
 
 
-at::Tensor add_custom(const at::Tensor& x, const at::Tensor& y) {
-  // construct the output tensor of the NPU
-  at::Tensor result = npu_preparation::apply_tensor_without_format(x);
+at::Tensor add_custom(const at::Tensor& x, const at::Tensor& y)
+{
+    // construct the output tensor of the NPU
+    at::Tensor result = npu_preparation::apply_tensor_without_format(x);
 
-  // calculate the output result of the NPU
-  EXEC_NPU_CMD(aclnnAddCustom, x, y);
-  return result;
+    // calculate the output result of the NPU
+    EXEC_NPU_CMD(aclnnAddCustom, x, y);
+    return result;
 }
 
-at::Tensor add_custom1(const at::Tensor& x, const at::Tensor& y) {
-  // construct the output tensor of the NPU
-  at::Tensor result = npu_preparation::apply_tensor_without_format(x);
+at::Tensor add_custom1(const at::Tensor& x, const at::Tensor& y)
+{
+    // construct the output tensor of the NPU
+    at::Tensor result = npu_preparation::apply_tensor_without_format(x);
 
-  // calculate the output result of the NPU
-  EXEC_NPU_CMD(aclnnAddCustom1, x, y);
-  return result;
+    // calculate the output result of the NPU
+    EXEC_NPU_CMD(aclnnAddCustom1, x, y);
+    return result;
 }
 
-at::Tensor add_custom2(const at::Tensor& x, const at::Tensor& y) {
-  // construct the output tensor of the NPU
-  at::Tensor result = npu_preparation::apply_tensor_without_format(x);
+at::Tensor add_custom2(const at::Tensor& x, const at::Tensor& y)
+{
+    // construct the output tensor of the NPU
+    at::Tensor result = npu_preparation::apply_tensor_without_format(x);
 
-  // calculate the output result of the NPU
-  EXEC_NPU_CMD(aclnnAddCustom2, x, y);
-  return result;
+    // calculate the output result of the NPU
+    EXEC_NPU_CMD(aclnnAddCustom2, x, y);
+    return result;
 }
 
-at::Tensor add_custom3(const at::Tensor& x, const at::Tensor& y) {
-  // construct the output tensor of the NPU
-  at::Tensor result = npu_preparation::apply_tensor_without_format(x);
+at::Tensor add_custom3(const at::Tensor& x, const at::Tensor& y)
+{
+    // construct the output tensor of the NPU
+    at::Tensor result = npu_preparation::apply_tensor_without_format(x);
 
-  // calculate the output result of the NPU
-  EXEC_NPU_CMD(aclnnAddCustom3, x, y);
-  return result;
+    // calculate the output result of the NPU
+    EXEC_NPU_CMD(aclnnAddCustom3, x, y);
+    return result;
 }
 
 }  // namespace op_api
